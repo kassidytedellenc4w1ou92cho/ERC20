6e7f4a3cfb482891a1d7c85faef1486f10eed6cb
kassidytedellenc4w1ou92cho <kassidytedellenc4w1ou92cho@hotmail.com>
2024-09-11 07:14:49 +0000

    Initialised

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("tt","tt"){} 
+    constructor()ERC20("ii","ii"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
